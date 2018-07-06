618caa312595f86a6a37992aefb37a66cc6e07a0
rraj <rraj@xavient.com>
2018-07-06 19:18:22 +0530

    now sensor works with dht11

--- simulatedSensor.js
+++ simulatedSensor.js
@@ -2,7 +2,7 @@
 * IoT Hub Raspberry Pi NodeJS - Microsoft Sample Code - Copyright (c) 2017 - Licensed MIT
 */
 'use strict';
-
+var dht = require('dht-sensor');
 function Sensor(/* options */) {
   // nothing todo
 }
@@ -13,9 +13,12 @@ Sensor.prototype.init = function (callback) {
 }
 
 Sensor.prototype.read = function (callback) {
+
+  var current = dht.read(11, 14); // 11 : DHT11, 18 : BCM GPIO
+  
   callback(null, {
-    temperature: random(20, 30),
-    humidity: random(60, 80)
+    temperature: current.temperature,
+    humidity: current.humidity
   });
 }
 
