65821de6e396186cd4895b4b241ed66d99687abc
rraj <rraj@xavient.com>
2018-07-06 19:37:13 +0530

    fixed issue with dht11

--- simulatedSensor.js
+++ simulatedSensor.js
@@ -6,15 +6,22 @@ var dht = require('dht-sensor');
 function Sensor(/* options */) {
   // nothing todo
 }
-
+var current = {
+  temperature:0,
+  humidity:0
+}
 Sensor.prototype.init = function (callback) {
   // nothing todo
   callback();
 }
 
 Sensor.prototype.read = function (callback) {
-
-  var current = dht.read(11, 14); // 11 : DHT11, 18 : BCM GPIO
+  try{
+    current = dht.read(11, 14); // 11 : DHT11, 18 : BCM GPIO
+  }catch(err){
+    console.log(err);
+  }
+  
   
   callback(null, {
     temperature: current.temperature,
